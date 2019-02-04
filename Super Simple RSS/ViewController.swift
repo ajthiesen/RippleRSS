50438fe4640d00a38b3e11b2557565d17e0fae9e
Geof Crowl <rectangular@gmail.com>
2019-02-03 18:12:50 -0700

    Start xcode project

---
+++ Super Simple RSS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Super Simple RSS
+//
+//  Created by Geof Crowl on 2/3/19.
+//  Copyright © 2019 Geof Crowl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
