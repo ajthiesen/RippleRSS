5f62f74022bc21823813f3d0bb019cab4899e203
Geof Crowl <rectangular@gmail.com>
2019-02-03 18:28:39 -0700

    Switch to storyboardless init

--- Super Simple RSS/AppDelegate.swift
+++ Super Simple RSS/AppDelegate.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 
+var appDelegate: AppDelegate?
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    override init() {
+        
+        super.init()
+        appDelegate = self
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Super Simple RSS/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Super Simple RSS
+//
+//  Created by Geof Crowl on 2/3/19.
+//  Copyright © 2019 Geof Crowl. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func loadView() {
+        
+        view = UIView()
+        view.backgroundColor = .red
+    }
+
+
+}
+
